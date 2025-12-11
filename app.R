60db294f99f1efa506d614d9a5bacf42e0839b72
webenjam-design <webenjam@umich.edu>
2025-12-11 18:47:24 -0500

    glm, select mult app.R

--- app.R
+++ app.R
@@ -3,6 +3,13 @@ library(tidyverse)
 
 load("spotify_mpd_01.RData")
 
+new_track_attr <- track_attr |> 
+  select(track_id, artists, track_name, danceability, energy, loudness,
+         acousticness, valence, tempo, track_genre) |>
+  drop_na()
+
+
+
 ui <- fluidPage(
   titlePanel("Search Bar with Dropdown Example"),
   
@@ -12,9 +19,9 @@ ui <- fluidPage(
       selectizeInput(
         inputId = "search_select",
         label = "Search and Select an Option:",
-        choices = c("", paste(track_attr$track_name, "by", track_attr$artists)), # 'state.name' is a built-in R dataset
+        choices = c("", paste(track_attr$track_name, "-", track_attr$artists)), # 'state.name' is a built-in R dataset
         selected = "",
-        multiple = FALSE, # Set to TRUE for multi-selection
+        multiple = T, # Set to TRUE for multi-selection
         options = list(
           placeholder = 'Start typing to search...',
           onInitialize = I('function() { this.setValue(""); }') # Optional: ensures placeholder is shown on load
@@ -24,7 +31,9 @@ ui <- fluidPage(
     
     mainPanel(
       h4("You selected:"),
-      verbatimTextOutput("selected_value")
+      verbatimTextOutput("selected_value"),
+      h4("Likes"),
+      tableOutput("resultsTable")
     )
   )
 )
@@ -33,6 +42,21 @@ server = function(input, output) {
   output$selected_value <- renderPrint({
     input$search_select
 })
+  output$resultsTable = renderTable({
+    user_input = new_track_attr |> filter(track_name %in% str_split_i(input$search_select, " - ", 1))
+    train_data <- new_track_attr |>
+      mutate(liked = ifelse(track_id %in% user_input$track_id, 1, 0)) |>
+      mutate(is_genre = ifelse(track_genre %in% user_input$track_genre, 1, 0))
+    preference_model <- glm(liked ~ danceability + energy + loudness + acousticness + valence + tempo + is_genre,
+                            data = train_data, family = binomial)
+    prediction_result <- train_data |>
+      ungroup() |>
+      mutate(predicted_score = predict(preference_model, newdata = train_data, type = "response")) |>
+      filter(!track_id %in% user_input$track_id) |> top_n(10, predicted_score) |> # remove input songs
+      arrange(desc(predicted_score)) # order
+    prediction_result
+  })
 }
 
 shinyApp(ui, server)
+
