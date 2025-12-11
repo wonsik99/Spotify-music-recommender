393ffd12159f13b9d29b72e0141ccc5bc38a2e1b
Benjamin Wei <webenjam@umich.edu>
2025-12-11 18:19:11 -0500

    search bar

--- app.R
+++ app.R
@@ -1,13 +1,38 @@
 library(shiny)
 library(tidyverse)
 
-ui = fluidPage({
+load("spotify_mpd_01.RData")
+
+ui <- fluidPage(
+  titlePanel("Search Bar with Dropdown Example"),
   
-})
+  sidebarLayout(
+    sidebarPanel(
+      # Use selectizeInput for the search/dropdown functionality
+      selectizeInput(
+        inputId = "search_select",
+        label = "Search and Select an Option:",
+        choices = c("", paste(track_attr$track_name, "by", track_attr$artists)), # 'state.name' is a built-in R dataset
+        selected = "",
+        multiple = FALSE, # Set to TRUE for multi-selection
+        options = list(
+          placeholder = 'Start typing to search...',
+          onInitialize = I('function() { this.setValue(""); }') # Optional: ensures placeholder is shown on load
+        )
+      )
+    ),
+    
+    mainPanel(
+      h4("You selected:"),
+      verbatimTextOutput("selected_value")
+    )
+  )
+)
 
 server = function(input, output) {
-  
+  output$selected_value <- renderPrint({
+    input$search_select
+})
 }
-
 
 shinyApp(ui, server)
