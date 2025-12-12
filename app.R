14971712dba10386cdc52e26d51e141bbb06ec89
Benjamin Wei <webenjam@umich.edu>
2025-12-11 22:36:41 -0500

    Radar chart

--- app.R
+++ app.R
@@ -26,7 +26,8 @@ ui <- fluidPage(
         multiple = T, # Set to TRUE for multi-selection
         options = list(
           placeholder = 'Start typing to search...',
-          onInitialize = I('function() { this.setValue(""); }') # Optional: ensures placeholder is shown on load
+          onInitialize = I('function() { this.setValue(""); }'), 
+          maxItems = 3
         )
       )
     ),
@@ -36,7 +37,8 @@ ui <- fluidPage(
       verbatimTextOutput("selected_value"),
       h4("Recommendations"),
       tableOutput("resultsTable"), 
-      uiOutput("chooseResultUi")
+      uiOutput("chooseResultUi"),
+      plotOutput("radarCharts")
     )
   )
 )
@@ -46,9 +48,15 @@ server = function(input, output) {
   output$selected_value <- renderPrint({
     input$search_select
 })
+  
+  user_input_rv = reactive({
+    new_track_attr |> filter(track_name %in% str_split_i(input$search_select, " - ", 1))
+  })
+  
+  #Stores the actual model
   resultList = reactive({
     req(input$search_select)
-    user_input = new_track_attr |> filter(track_name %in% str_split_i(input$search_select, " - ", 1))
+    user_input = user_input_rv()
     train_data <- new_track_attr |>
       mutate(liked = ifelse(track_id %in% user_input$track_id, 1, 0)) |>
       mutate(is_genre = ifelse(track_genre %in% user_input$track_genre, 1, 0))
@@ -71,7 +79,32 @@ server = function(input, output) {
   output$chooseResultUi = renderUI({
     options = resultList()
     selectInput("resultSelect", "Select Result", choices = options$track_name)
+                #choices = paste(options$track_name, " - ", options$artists))
   })
+  
+  output$radarCharts = renderPlot({
+    req(input$resultSelect)
+    used_colors = c("red", "green", "blue", "black")
+    resultInput = input$resultSelect
+    compared = resultList()
+    compared = compared |> filter(track_name == resultInput) |> select(-liked, -is_genre, -predicted_score)
+                                      #str_split_i(resultInput, " - ", 1))
+    user_input = user_input_rv()
+    compared = rbind(compared, user_input)
+    maxes = apply(compared, 2, max)
+    maxrow = as.data.frame(t(maxes))
+    mins = apply(compared, 2, min)
+    minrow = as.data.frame(t(mins))
+    max_min = rbind(maxrow, minrow)
+    #rownames(max_min) = c("Max", "Min")
+    max_min$track_name = c("Max", "Min")
+    compared = rbind(max_min, compared) |> select(track_name, danceability, energy, loudness,  acousticness, valence, tempo) |>
+      mutate(across(-track_name, as.numeric)) |> column_to_rownames("track_name")
+    radarchart(compared, pcol = used_colors)
+    legend(x = "left", legend = rownames(compared[-c(1, 2),]),
+           bty = "n", pch = 20, col = used_colors)
+  })
+  
   
 }
 
